5e982177ec70dd3b31f0d5817e79dbfa66a292f8
Hector Romero <hectorromero@Hectors-MacBook-Pro.local>
2016-05-14 11:09:52 -0700

    auth0 add cors

--- index.js
+++ index.js
@@ -9,12 +9,31 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 // Create the application
 var app = express();
 
+// Add Auth0
+var jwt = require('express-jwt');
+var cors = require('cors');
+
 // Add Middleware necessary for REST API's - Intercept itself into the request
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
-// CORS Support
+// Add Auth0
+app.use(cors());
+
+var authCheck = jwt({
+    secret: new Buffer('Z5PvnaRqjKBr3JudkhnXHAq4t_GbLBXE6thziX87Wk9w_v6-hX2hycZ72aabmq3D','base64'),
+    audience: 'Vd2v0braxVQxH0lASGPNci2LlthA8fgn'
+})
+
+app.get('/api/public', function(req,res){
+    res.json({ message: "Hello from the PUBLIC endpoint! You don't need to be authenticated"});
+});
+app.get('/api/private', authCheck, function(req,res){
+    res.json({ message: "Hello from the PRIVATE endpoint! You DO need to be authenticated"});
+});
+
+// CORS Support (Non-Auth0)
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
